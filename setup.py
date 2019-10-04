013d7e88eef77fca609f8ad006f6fe05646eeb56
Faizal Sidek <faizalsidek@gmail.com>
2019-10-04 11:43:16 +0800

    Update setup

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 setuptools.setup(
   name = 'pyazdvops',
-  version = '1.0.1',
+  version = '1.0.2',
   description = 'Library for Azure DevOps API for Python',
-  author = 'M Faizal Sidek',
-  author_email = 'mfaizal.sidek@petronas.com.my',
-  license = 'MIT',
+  author = 'Faizal Sidek',
+  author_email = 'faizal.sidek@gmail.com',
+  license = 'Free for non-commercial use',
   packages = ['azdvop'],
   zip_safe = False,
   install_requires = ['requests'],
@@ -16,7 +16,8 @@ setuptools.setup(
     'Development Status :: 3 - Alpha', 
     'Intended Audience :: Developers', 
     'Topic :: Software Development :: Libraries',
-    'License :: OSI Approved :: MIT License',
+    'License :: Free for non-commercial use',
+    'Natural Language :: English',
     'Programming Language :: Python :: 2.7'
   ]
 )
